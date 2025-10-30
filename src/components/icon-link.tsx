52fac726c07a73f08e56b2d9c7a03ea6a0d175f8
Lucas Pereira <lucas4162007@gmail.com>
2025-10-30 18:29:45 -0300

    chore: Adiciona Icones

---
+++ src/components/icon-link.tsx
@@ -0,0 +1,43 @@
+import { cva, type VariantProps } from "cva";
+import Icon from "./icon";
+
+export const IconLinkVariants = cva("", {
+	variants: {
+		disabled: {
+			true: "pointer-events-none",
+		},
+	},
+	defaultVariants: {
+		disabled: false,
+	},
+});
+
+interface IconLinkProps
+	extends Omit<React.ComponentProps<"a">, "disabled">,
+		VariantProps<typeof IconLinkVariants> {
+	url: string;
+	icon: React.ComponentProps<typeof Icon>["svg"];
+	aria_label: string;
+}
+
+export default function IconLink({
+	icon,
+	url,
+	className,
+	disabled,
+	aria_label,
+	...props
+}: IconLinkProps) {
+	return (
+		<a
+			href={url}
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label={aria_label}
+			className={IconLinkVariants({ disabled, className })}
+			{...props}
+		>
+			<Icon svg={icon} />
+		</a>
+	);
+}
---
+++ src/components/icon.tsx
@@ -0,0 +1,37 @@
+import { cva, type VariantProps } from "cva";
+
+export const iconVariants = cva("", {
+	variants: {
+		size: {
+			sm: "w-5 h-5",
+		},
+		animate: {
+			true: "animate-spin",
+		},
+	},
+	defaultVariants: {
+		size: "sm",
+		animate: false,
+	},
+});
+
+interface IconProps
+	extends React.ComponentProps<"svg">,
+		VariantProps<typeof iconVariants> {
+	svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+export default function Icon({
+	svg: SvgComponent,
+	className,
+	animate,
+	size,
+	...props
+}: IconProps) {
+	return (
+		<SvgComponent
+			className={iconVariants({ animate, size, className })}
+			{...props}
+		></SvgComponent>
+	);
+}
