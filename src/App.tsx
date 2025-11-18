89c37262b841a3ac9ba0072d7160e04079ca7bc1
Lucas Pereira <lucas4162007@gmail.com>
2025-11-18 19:41:27 -0300

    chore: Adiciona `Zod`

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./App.css";
-import DigitalClock from "./components/digitalClock";
+
 import Starry from "./components/starry";
-import Home from "./pages/home";
+import Toolbar from "./components/toolbar";
+import Home from "./core-componets/home";
+import Info from "./core-componets/info";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<Starry />
-			<DigitalClock />
+			<Info />
+			<Toolbar />
 
 			<main className="h-full">
 				<Home />
 			</main>
-		</>
+
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
