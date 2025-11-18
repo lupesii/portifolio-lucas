923f056123d40f1c1657ef66a5cdf7b5d8f4f649
Lucas Pereira <lucas4162007@gmail.com>
2025-11-18 19:48:19 -0300

    feat: Adiciona componente `WeatherCard`

---
+++ src/components/weather-card.tsx
@@ -0,0 +1,62 @@
+import { cva, type VariantProps } from "cva";
+import HumidityIcon from "../assets/icons/humidity.svg?react";
+import ThermometerIcon from "../assets/icons/thermometer.svg?react";
+import WindIcon from "../assets/icons/wind.svg?react";
+import { useWeather } from "../hooks/use-weather";
+import Icon from "./icon";
+import Text from "./text";
+
+export const weatherCardVariants = cva(
+	`flex flex-col justify-center items-center gap-2 w-max h-ma
+	p-2 rounded-lg bg-preto border-cinza-300 border pointer-events-none
+	transition-all duration-200`,
+	{
+		variants: {
+			visibility: {
+				false: "opacity-0",
+				true: "opacity-100",
+			},
+		},
+		defaultVariants: {
+			visibility: false,
+		},
+	},
+);
+
+interface WeatherCardProps
+	extends VariantProps<typeof weatherCardVariants>,
+		React.ComponentProps<"div"> {}
+
+export default function WeatherCard({
+	visibility,
+	className,
+}: WeatherCardProps) {
+	const { data } = useWeather();
+
+	return (
+		<aside className={weatherCardVariants({ visibility, className })}>
+			<Text variant="albert-md-bold">S. J. Dos Campos, SP</Text>
+			<div className="flex items-center gap-1">
+				<Icon svg={ThermometerIcon} />
+				<Text variant="anony-sm-bold" className="text-xl">
+					{data?.current.temp_c}°C
+				</Text>
+			</div>
+			<Text variant="anony-sm">{data?.current.condition.text}</Text>
+			<div className="flex flex-col gap-1 self-start">
+				<div className="flex items-center gap-1">
+					<Icon svg={HumidityIcon} size="none" className="w-4 h-4" />
+					<Text variant="none" className="font-mono text-[13px]">
+						{data?.current.humidity}% humidity
+					</Text>
+				</div>
+				<div className="flex items-center gap-2">
+					<Icon svg={WindIcon} size="none" className="w-4 h-4" />
+					<Text variant="none" className="font-mono text-[13px]">
+						{data?.current.wind_kph} km/h
+					</Text>
+				</div>
+			</div>
+		</aside>
+	);
+}
