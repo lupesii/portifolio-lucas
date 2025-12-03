ea312bf9066663f93dd8d6f0e387d908931a44d4
Lucas Pereira <lucas4162007@gmail.com>
2025-12-03 13:52:11 -0300

    feat: Add component `Form`

---
+++ src/components/form.tsx
@@ -0,0 +1,46 @@
+import type { FormEvent } from "react";
+import SendIcon from "../assets/icons/send.svg?react";
+import Icon from "./icon";
+import InputBox from "./input-box";
+import Text from "./text";
+
+export default function Form() {
+	function handleSubmitForm(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		alert("Calma, falta programar");
+	}
+
+	return (
+		<form
+			onSubmit={handleSubmitForm}
+			className="bg-cinza-400 border border-azul space-y-6.25 py-6 px-5 rounded-[10px] w-3/6"
+		>
+			<Text as="h1" variant="anony-md-bold" className="mb-6.25" trace>
+				Lets build something together!
+			</Text>
+
+			<div className="flex items-center gap-5">
+				<InputBox id="Name" placeholder="Your Name" className="w-full" />
+				<InputBox
+					id="Email"
+					placeholder="Your email address"
+					className="w-full"
+				/>
+			</div>
+			<InputBox id="Subject" placeholder="Subject of your message" />
+			<InputBox
+				as="textarea"
+				id="Message"
+				placeholder="What I can help you with?"
+			/>
+
+			<button
+				type="submit"
+				className="flex items-center justify-center gap-3 bg-azul w-full p-5 rounded-[10px]"
+			>
+				<Icon svg={SendIcon} />
+				<Text variant="anony-2xl-bold">Send Message</Text>
+			</button>
+		</form>
+	);
+}
