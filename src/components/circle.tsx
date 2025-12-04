33853c3228909cc2a9fdac9865f6e3355966bb18
Lucas Pereira <lucas4162007@gmail.com>
2025-12-04 15:46:14 -0300

    feat: Passa propriedades ao`CIrcle`

--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -1,4 +1,5 @@
 import { cva, type VariantProps } from "cva";
+import type { ComponentProps } from "react";
 
 export const circleVariants = cva("rounded-full inline-block", {
 	variants: {
@@ -6,10 +7,13 @@ export const circleVariants = cva("rounded-full inline-block", {
 			none: "",
 			green: "bg-verde",
 			blue: "bg-azul",
+			cinza: "bg-cinza-400",
 		},
 		size: {
 			sm: "w-1.25 h-1.25",
 			lg: "w-2.5 h-2.5",
+			md: "w-2 h-2",
+			xlg: "w-4 h-4",
 		},
 	},
 	defaultVariants: {
@@ -18,10 +22,22 @@ export const circleVariants = cva("rounded-full inline-block", {
 	},
 });
 
-interface CircleProps extends VariantProps<typeof circleVariants> {
+interface CircleProps
+	extends Omit<ComponentProps<"div">, "size" | "color">,
+		VariantProps<typeof circleVariants> {
 	className?: string;
 }
 
-export default function Circle({ color, size, className }: CircleProps) {
-	return <div className={circleVariants({ color, size, className })}></div>;
+export default function Circle({
+	color,
+	size,
+	className,
+	...props
+}: CircleProps) {
+	return (
+		<div
+			{...props}
+			className={circleVariants({ color, size, className })}
+		></div>
+	);
 }
